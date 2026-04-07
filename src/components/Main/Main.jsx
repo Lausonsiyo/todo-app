bcae081f72c611317c150750eaebd7962ac08d02
luxondev <andreslauson@gmail.com>
2026-04-07 01:17:56 -0600

    feture filter tasks

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -11,12 +11,13 @@ import {
 import "./Main.css";
 
 //COMPONENT IMPORTS
-import TaskList from "../TaskList/TaskLisk";
+import TaskList from "../TaskList/TaskList";
 
 function Main() {
   //INITIAL STATE FOR TASK INPUT AND TASK LIST
   const [task, setTask] = useState("");
-  const [tasks, setTasks] = useState(getTasksFromLocalStorage());
+  const [tasks, setTasks] = useState(() => getTasksFromLocalStorage());
+  const [filter, setFilter] = useState("all");
 
   //FUNCTION TO HANDLE ADDING A NEW TASK
   const handleAddTask = () => {
@@ -32,13 +33,20 @@ function Main() {
   };
 
   //FUNCTION TO HANDLE COMPLETE/TOGGLE TASK
-  const handdleToggleTask = (id) => {
+  const handleToggleTask = (id) => {
     setTasks(
       tasks.map((task) =>
         task.id === id ? { ...task, completed: !task.completed } : task,
       ),
     );
   };
+
+  //FUNCTION TO CREATE FILTERED TASKS
+  const filteredTasks = tasks.filter((task) => {
+    if (filter === "active") return !task.completed;
+    if (filter === "completed") return task.completed;
+    return true;
+  });
 
   //USE EFFECT TO SAVE TASKS TO LOCAL STORAGE WHENEVER TASKS STATE CHANGES
   useEffect(() => {
@@ -60,11 +68,17 @@ function Main() {
           Add Task
         </button>
       </div>
+      <div className="filters">
+        <button onClick={() => setFilter("all")}>All</button>
+        <button onClick={() => setFilter("active")}>Active</button>
+        <button onClick={() => setFilter("completed")}>Completed</button>
+      </div>
       <TaskList
+        key={task.id}
         title="My Tasks"
-        tasks={tasks}
+        tasks={filteredTasks}
         onDeleteTask={handleDeleteTask}
-        onToggleTask={handdleToggleTask}
+        onToggleTask={handleToggleTask}
       />
     </main>
   );
--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -1,6 +1,6 @@
 function TaskItem({ task, onDeleteTask, onToggleTask }) {
   return (
-    <li key={task.id} className="task__list-item">
+    <li className="task__list-item">
       <p
         className={`task__title ${task.completed ? "task__title-completed" : ""}`}
         onClick={() => onToggleTask(task.id)}
